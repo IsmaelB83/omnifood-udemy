07019489cec78bf58889fe7afa275d65b3bcd43a
Ismael Bernal <ismaelbernal83@gmail.com>
2018-07-12 19:56:02 +0200

    Adding animations on scroll with animate.css

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         }
     }, {
             offset: '60px;'
-        })
+    })
 
     /* Scroll on buttons */
     $('.js--scroll-to-plans').click(function () {
@@ -62,5 +62,30 @@ $(document).ready(function () {
             }
         }
     );
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
 
 });
